ebe39b19e390b39c93a6ac0c63380741ba7c4089
ydh6226 <ydh6226@naver.com>
2021-08-12 22:59:57 +0900

    스프링 인터셉터 요청 로그 남기기

--- login/src/main/java/hello/login/web/filter/LoginCheckFilter.java
+++ login/src/main/java/hello/login/web/filter/LoginCheckFilter.java
@@ -13,7 +13,7 @@ import java.io.IOException;
 @Slf4j
 public class LoginCheckFilter implements Filter {
 
-    private static final String[] whitelist = {"/", "/members/add", "/login", "logout", "/css/*"};
+    private static final String[] whitelist = {"/", "/members/add", "/login", "/logout", "/css/*"};
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
