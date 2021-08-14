b0d2067ca42f6e60bdb75f908e7ba3c234cc53cf
ydh6226 <ydh6226@naver.com>
2021-08-14 23:55:50 +0900

    ArgumentResolver로 회원객체 가져오기

--- login/src/main/java/hello/login/web/HomeController.java
+++ login/src/main/java/hello/login/web/HomeController.java
@@ -2,6 +2,7 @@ package hello.login.web;
 
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
+import hello.login.web.argumentResolver.Login;
 import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -76,7 +77,7 @@ public class HomeController {
     }
 
     //SessionAttribute는 세션을 생성하진 않는다.
-    @GetMapping("/")
+//    @GetMapping("/")
     public String homeLoginV4(
             @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model) {
         if (loginMember == null) {
@@ -86,5 +87,16 @@ public class HomeController {
         model.addAttribute("member", loginMember);
         return "loginHome";
     }
+
+    @GetMapping("/")
+    public String homeLoginArgumentResolver(@Login Member loginMember, Model model) {
+        if (loginMember == null) {
+                return "home";
+        }
+
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+
 
 }
