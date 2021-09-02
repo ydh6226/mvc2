f6a8262dd9856af98314bfcc406efdf5978fc19f
ydh6226 <ydh6226@naver.com>
2021-09-02 20:17:27 +0900

    type converter 프로젝트 생성

---
+++ typeconverter/src/main/java/hello/typeconverter/TypeApplication.java
@@ -0,0 +1,11 @@
+package hello.typeconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TypeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TypeApplication.class, args);
+    }
+}
